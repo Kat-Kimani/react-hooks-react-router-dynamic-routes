9e14f6bad76df41093195547d774613353e665a5
catherine <wachukimani@gmail.com>
2023-04-26 10:12:05 +0300

    completed lab

---
+++ src/components/MovieShow.js
@@ -0,0 +1,33 @@
+// // ./src/components/MovieShow.js
+// import React from "react";
+
+// function MovieShow() {
+//   return (
+//     <div>
+//       <h3>Movies Show Component!</h3>
+//     </div>
+//   );
+// }
+
+// export default MovieShow;
+/////////////////////////////////////////////////////////////
+
+// .src/components/MovieShow.js
+import React from "react";
+import { useParams } from "react-router-dom";
+
+function MovieShow({ movies }) {
+  // call useParams to access the `params` from the url
+  const params = useParams();
+  console.log(params);
+
+  return (
+    <div>
+      {/* And here we access the `movieId` stored in `params` to render 
+        information about the selected movie */}
+      <h3>{movies[params.movieId].title}</h3>
+    </div>
+  );
+}
+
+export default MovieShow;
